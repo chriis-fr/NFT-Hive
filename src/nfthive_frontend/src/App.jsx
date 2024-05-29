015809ea3c901b2fab6504bfb4333f0207d98281
chriis-fr <caspianodhis@gamil.com>
2024-05-30 01:02:46 +0300

    night night

--- src/nfthive_frontend/src/App.jsx
+++ src/nfthive_frontend/src/App.jsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
-import { nfthive_backend } from 'declarations/nfthive_backend';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from "./components/Header";
 
-function App() {
-  const [greeting, setGreeting] = useState('');
+const App = () => {
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    nfthive_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  return(
+    <div className="App">
+      <Header />
+    </div>
+  )
 
-  return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
 }
 
 export default App;
--- src/nfthive_frontend/src/main.jsx
+++ src/nfthive_frontend/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import { Principal } from "@dfinity/principal"
 
+const CURRENT_USER_ID = Principal.fromText("2vxsx-fae");
+export default CURRENT_USER_ID;
+
+const init = async () => {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+};
+
+init();
